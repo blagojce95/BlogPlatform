daf0d379a5dad58d838358e28baf8cf959d8e2f6
blagojce95 <blagojce95@gmail.com>
2016-03-01 20:51:28 +0100

    Creating the project

---
+++ BlogPlatformProject/BlogPlatformProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlogPlatformProject.Startup))]
+namespace BlogPlatformProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
